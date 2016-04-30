58d37750ba05188d0931ab2fd8fcc1db1386d281
Andrew Connell <me@andrewconnell.com>
2016-04-30 06:00:05 -0400

    add all files used in my deployment
    Added all files that are used in my custom deployment. This includes 3rd party modules, themes & monitoring (newrelic).
    
    References #2.

---
+++ Themes/MsCloudShow/Filters/LayoutFilter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Orchard;
+using Orchard.Mvc.Filters;
+
+namespace MetroMan.Filters {
+  public class LayoutFilter : FilterProvider, IResultFilter {
+    private readonly IWorkContextAccessor _wca;
+
+
+    public LayoutFilter(IWorkContextAccessor wca) {
+      _wca = wca;
+    }
+
+
+    public void OnResultExecuting(ResultExecutingContext filterContext) {
+      WorkContext workContext = _wca.GetContext();
+      RouteValueDictionary routeValues = filterContext.RouteData.Values;
+
+      string area = workContext.HttpContext.Request.Path;
+
+      if (area == "/") {
+        workContext.Layout.Metadata.Alternates.Add("Layout__Home");
+      }
+      if (area.Contains("/Users/Account/LogOn") || area.Contains("/Users/Account/AccessDenied") || area.Contains("/Users/Account/Register")) {
+        workContext.Layout.Metadata.Alternates.Add("Layout__LogonRegister");
+      }
+    }
+
+
+    public void OnResultExecuted(ResultExecutedContext filterContext) { }
+  }
+}
