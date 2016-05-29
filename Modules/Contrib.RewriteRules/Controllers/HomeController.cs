afeb230d1ed4137014355f4e58fe7cb408267610
Andrew Connell <me@andrewconnell.com>
2016-05-28 21:26:39 -0400

    fix(theme): update Orchard.RewriteRules to address unthemed 404 and 500 pages
    There was an issue where the Orchard.RewriteRules module didn't theme the rendered error pages.
    
    References #10.

--- Modules/Contrib.RewriteRules/Controllers/HomeController.cs
+++ Modules/Contrib.RewriteRules/Controllers/HomeController.cs
@@ -1,8 +1,9 @@
-﻿using System.Web.Mvc;
+﻿using Orchard.Themes;
+using System.Web.Mvc; 
 
 namespace Contrib.RewriteRules.Controllers {
+    [Themed]
     public class HomeController : Controller {
-
         public ActionResult Rewrite(string path) {
             return new HttpNotFoundResult();
         }
